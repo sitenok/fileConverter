8e7d81ec1ab8452d2c5fd560a7d0151d1522fcc6
sitenok <nicolesitenok@gmail.com>
2022-05-26 12:57:54 +0900

    samplefiles

--- yamlToJson/convertFile.py
+++ yamlToJson/convertFile.py
@@ -1,4 +1,4 @@
-## Python Script to convert YAML to JSON
+## Python Program to convert YAML to JSON and JSON to YAML
 import sys
 import yaml
 import json
@@ -7,29 +7,33 @@ import json
 filein = sys.argv[1]
 fileout = sys.argv[2]
 
-if( ( filein.endswith('.yml') ) or ( filein.endswith('.yaml') )):
-    print ("\nYou would like to convert", filein, end=' ')
-    print ("to", fileout, "\n\n")
-    print ("YAML file to JSON file.\n\n")
-
-if( ( filein.endswith('.jsn') ) or ( filein.endswith('.json') )):
-    print ("\nYou would like to convert", filein, end=' ')
-    print ("to", fileout)
-    print ("JSON file to YAML file.\n\n")
-
-
 # List to hold the data from file
 data = {}
 
+# Function to convert JSON file to YAML file
 def create_yaml():
     # Read the YAML file & load to data list
     with open(filein) as infile:
-        data = yaml.safe_load(infile, Loader=yaml.FullLoader)     
+        data = yaml.load(infile, Loader=yaml.FullLoader)     
         print(data)
 
     #Open a file to write the JSON output. The 'w' makes the file writable
     with open(fileout, 'w') as outfile:
         json.dump(data, outfile, indent=4) 
         print("JSON file written.")
+
+
+# YAML >> JSON
+if( ( filein.endswith('.yml') ) or ( filein.endswith('.yaml') )):
+    print ("\nYou would like to convert", filein, end=' ')
+    print ("to", fileout, "\n\n")
+    print ("YAML file to JSON file.\n\n")
+
+# JSON >> YAML
+if( ( filein.endswith('.jsn') ) or ( filein.endswith('.json') )):
+    print ("\nYou would like to convert", filein, end=' ')
+    print ("to", fileout)
+    print ("JSON file to YAML file.\n\n")
+    create_yaml()
 
 
